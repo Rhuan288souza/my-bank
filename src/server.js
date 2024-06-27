3318b21f27030036484d6c8f439ade7524ce0afe
Rhuan Souza <rhuan288souza@gmail.com>
2024-06-27 16:36:15 -0300

    adjust server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const Koa = require('koa')
 const { ApolloServer } = require('apollo-server-koa')
-const { ApolloServerPluginLandingPageDisabled } = require('apollo-server-core')
 const bodyParser = require('koa-bodyparser')
 const { makeExecutableSchema } = require('@graphql-tools/schema')
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core')
 const connectDB = require('../config/database')
 const typeDefs = require('./schemas')
 const resolvers = require('./resolvers')
@@ -14,16 +14,28 @@ connectDB()
 
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
-const server = new ApolloServer({ 
+const server = new ApolloServer({
   schema,
-  plugins: [ApolloServerPluginLandingPageDisabled()],
+  introspection: true,
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground({}),
+  ]
 })
 
 async function startServer() {
-  await server.start()
-  server.applyMiddleware({ app })
-
+  await server.start();
+  server.applyMiddleware({ app });
   app.use(bodyParser())
+
+  // Verify routes
+  app.use(async (ctx, next) => {
+    if (ctx.path === '/graphql' || ctx.path === '/') {
+      await next();
+    } else {
+      ctx.status = 404;
+      ctx.body = 'Not Found'
+    }
+  })
 
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}${server.graphqlPath}`)
